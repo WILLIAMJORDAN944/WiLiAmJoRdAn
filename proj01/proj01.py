a969bd43f26b2904daa3e857d0d8028449469974
WILLIAMJORDAN944 <williamjordan944@gmail.com>
2017-07-10 15:42:56 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,10 +1,19 @@
-# Name:
-# Date:
+# Name:William Jordan
+# age:12
 
-# proj01: A Simple Program
-# This program asks the user for his/her name and age.
-# Then, it prints a sentence that says when the user will turn 100.
 
+# proj01: A Simple sentence that says when the user will turn 100.
 # If you complete extensions, describe your extensions here!
 
+name=raw_input("Enter your name:")
+age=raw_input("Enter your age:")
+# Then, it prints
 
+
+result=2017-int(age)+100
+
+print(name),"you will turn 100 in year",(result)
+
+print "THANK YOU!  :D"
+#this program asks the user for his/her name and age.during the year",(result)
+# print age>100, "i can predict your age since you are older than 100"
